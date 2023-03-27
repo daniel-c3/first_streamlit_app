7d4419d660f192803226ea744560d05d76af9081
daniel-c3 <129103098+daniel-c3@users.noreply.github.com>
2023-03-27 12:34:04 -0600

    Update streamlit_app.py
    added a subtitle and a menu of options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
-import streamlit
+import streamlit as st
 
-streamlit.title("My Parents New Healthy Diner")
+st.title("My Parents New Healthy Diner")
+
+st.subheader("Breakfast Menu")
+
+option1 = "Omega 3 & Blueberry Oatmeal"
+option2 = "Kale, Spinach & Rocket Smoothie"
+option3 = "Hard-Boiled Free-Range Egg"
+
+option = st.selectbox("Selecciona una opción", [option1, option2, option3])
+
+st.write("Has seleccionado:", option)
