86e5bbda6537ba9868dff30775fdab0b14a4d061
daniel-c3 <129103098+daniel-c3@users.noreply.github.com>
2023-03-27 13:17:59 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,7 @@ st.text( " 🥑🍞 Avocado Toast" )
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+import pandas as pd
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
